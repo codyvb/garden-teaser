c3de3a52b89de9e11c20c115c83815bd2d2cf5d2
Codyvb <37961637+codyvb@users.noreply.github.com>
2023-06-07 13:01:47 -0600

    coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-between p-24">
       
-      <div className='text-2xl mt-auto mb-auto font-pixeloid'>
+      <div className='text-2xl mt-auto font-pixeloid'>
       The Garden
+      </div>
+
+      <div className='text-md mt-4 mb-auto font-pixeloid'>
+      Coming Soon
       </div>
 
     </main>
