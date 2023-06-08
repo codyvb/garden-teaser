205557b6f0a34496b804451b129bca0a4e5a3efd
Codyvb <37961637+codyvb@users.noreply.github.com>
2023-06-07 22:56:03 -0600

    earf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import Image from 'next/image'
+import Link from 'next/image'
+
 
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-between p-24">
+
+      <div className='mt-[20%]'>
+      <div className="w-[150px] h-[150px] bg-cover bg-[url('/earf.png')]"></div>
       
-      <div className='text-2xl mt-auto font-pixeloid'>
+      <div className='text-2xl font-pixeloid'>
       The Garden
       </div>
 
-      <div className='text-md mt-4 mb-auto font-pixeloid'>
-      Coming Soon
+      <div className='text-md mt-4 text-center mb-auto font-pixeloid'>
+      <a href='https://twitter.com/gardenwtf'>Coming Soon</a>
+      </div>
       </div>
 
     </main>
